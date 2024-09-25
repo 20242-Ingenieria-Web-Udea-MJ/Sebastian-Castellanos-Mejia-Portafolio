5cd213a7414a9fdd3de580ce613624246f11f653
SebastianCastellanosM <scastellanos.mejia@udea.edu.co>
2024-09-25 13:56:01 -0500

    titulos

--- src/components/atoms/titles/index.tsx
+++ src/components/atoms/titles/index.tsx
@@ -11,7 +11,7 @@ const TitlePrincipal = ({ title }: { title: string }) => {
 
 const TitleSecondary = ({ title }: { title: string }) => {
   return (
-    <h1 className='font-light text-3xl text-center text-tertiary sm:text-4xl sm:text-left '>
+    <h1 className="font-inter font-bold text-[32px] leading-[39.55px] text-left w-[232px] h-[40px] absolute top-[509.09px] left-[387px] opacity-0">
       {title}
     </h1>
   );
@@ -19,14 +19,14 @@ const TitleSecondary = ({ title }: { title: string }) => {
 
 const TitleTertiary = ({ title }: { title: string }) => {
   return (
-    <h1 className='font-light text-xl text-center text-tertiary sm:text-2xl sm:text-left '>
+    <h1 className='font-inter font-medium text-[18px] leading-[22.25px] text-left w-[158px] h-[22px] absolute top-[814.09px] left-[85px] opacity-0 '>
       {title}
     </h1>
   );
 };
 
 const TextPrincipal = ({ text }: { text: string }) => {
-  return <p className='text-appGray text-center text-base sm:text-lg sm:text-left'>{text}</p>;
+  return <p className='font-inter font-normal text-[15px] leading-[24px] text-center w-[438px] h-[68px] absolute top-[574.09px] left-[275px] opacity-0'>{text}</p>;
 };
 
 export { TitlePrincipal, TitleSecondary, TitleTertiary };
