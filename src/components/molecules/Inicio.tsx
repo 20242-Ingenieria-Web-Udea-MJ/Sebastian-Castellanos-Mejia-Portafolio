ddb2fd79a735fd0236a4cd651aab77d49b89ec09
SebastianCastellanosM <scastellanos.mejia@udea.edu.co>
2024-10-01 09:32:26 -0500

    Actualizacion Fotos

--- src/components/molecules/Inicio.tsx
+++ src/components/molecules/Inicio.tsx
@@ -5,16 +5,19 @@ import React from 'react'
 
 const Inicio = () => {
   return (
-    <div className='flex flex-col items-center justify-center w-[970px] h-[467px] gap-4 bg-white relative'>
-      <div className='flex flex-col items-center  gap-4'>
-       <TitlePrincipal>
-           I’m Sebastián Castellanos<span className="text-primary"> Front-end</span> Developer
+    <div className='flex items-center justify-between w-[970px] h-[467px] gap-4 bg-white relative'>
+      <div className='flex flex-col items-center gap-4'>
+        <TitlePrincipal>
+          I’m Sebastián Castellanos<span className="text-primary"> Front-end</span> Developer
         </TitlePrincipal>
-        <TextPrincipal text={'Soy un estudiante de Ingeniería de Sistemas de la Universidad de Antioquia con un fuerte interés en la ciberseguridad y el desarrollo Full Stack. Me apasiona aprender nuevas tecnologías y metodologías ágiles para el desarrollo de aplicaciones web, así como implementar soluciones seguras y escalables. '} />
+        <TextPrincipal text={'Soy un estudiante de Ingeniería de Sistemas de la Universidad de Antioquia con un fuerte interés en la ciberseguridad y el desarrollo Full Stack. Me apasiona aprender nuevas tecnologías y metodologías ágiles para el desarrollo de aplicaciones web, así como implementar soluciones seguras y escalables.'} />
         <ButtonPrincipal/>
-       </div>
+      </div>
+      {/* Agrega la imagen aquí */}
+      <div>
+        <img src='/images/Foto.jpeg' alt='Sebastián Castellanos' className='w-[390px] h-auto' />
+      </div>
     </div> 
-       
   );
 }
 
